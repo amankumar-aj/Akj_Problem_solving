cb246b60e01bdebb4fa3ee2319fe0bb52a9582ae
amankumar-aj <amankumar_aj@outlook.com>
2024-11-13 21:34:21 +0530

    p12 updated

--- P12_P13_HackerRank_CQ/P12_HKR_lowest_triangle.c
+++ P12_P13_HackerRank_CQ/P12_HKR_lowest_triangle.c
@@ -1,2 +1,20 @@
 
 // https://www.hackerrank.com/challenges/lowest-triangle/problem?isFullScreen=true
+/*
+
+ * Complete the 'lowestTriangle' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER trianglebase
+ *  2. INTEGER area
+
+//solution
+int lowestTriangle(int trianglebase, int area) {
+    int h=(int)ceil((2.0 * area) /trianglebase);
+
+    return h;
+
+}
+
+ */
